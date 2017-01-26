421371cce00ec3592f51cda52fac8b38612624b6
Richard Strauss <rstrauss91@gmail.com>
2017-01-26 13:53:27 -0500

    Create edit contact functionality

--- crm.rb
+++ crm.rb
@@ -53,10 +53,13 @@ end
 put "/contacts/:id" do
   @contact = Contact.find(params[:id].to_i)
   if @contact
-    @contact.first_name = params[:first_name]
-    @contact.last_name = params[:last_name]
-    @contact.email = params[:email]
-    @contact.note = params[:note]
+    @contact = Contact.update(
+    @contact.id,
+    first_name: params[:first_name],
+    last_name: params[:last_name],
+    email: params[:email],
+    note: params[:note]
+    )
 
     redirect to("/contacts")
   else
